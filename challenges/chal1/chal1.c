6ed5599c1f61b549238a5a78dfe80002fdcd75f3
Carlos Cabello <carloscabello364@gmail.com>
2019-01-15 20:53:13 -0600

    Update chal1.c

--- challenges/chal1/chal1.c
+++ challenges/chal1/chal1.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
+void ex1(char* list)
+{
+    int c = 0;
+    int c2 = 0;
+    char list2[40];
+    int hasLetter = 0;
 
+    
+    for(int i = 0; i < sizeof(list)/sizeof(list[0]); i++)
+    {
+        hasLetter=0;
+        for(int j = 0; j < sizeof(list2)/sizeof(list2[0]); j++)
+        {
+            if((list[i] == list2[j]) && !hasLetter)
+            {
+                hasLetter = 1;
+            }
+        }
+        if(!hasLetter)
+        {
+            list2[c] = list[i];
+        }
+        c++;
+    } 
+    printf("%s\n", list2);
+}
+
+int main()
+{
+    ex1("pwwkew");
+}
