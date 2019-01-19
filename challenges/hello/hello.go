3aa05210765af3f861dc9281a23030c2a0e19701
Carlos <carloscabello364@gmail.com>
2019-01-18 19:59:24 -0600

    Excercise 1-23 remove comments

--- challenges/hello/hello.go
+++ challenges/hello/hello.go
@@ -4,22 +4,45 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"strings"
 	"time"
 )
 
 const pi = math.Pi
 const byt uint8 = 0
 
-//Type with no capital letter is private
+//Declaration with no capital letter is private
 type vector struct {
 	magnitude int
 	direction int
 }
 
-//Vertex Type with capital letter is public
+//Vertex Declaration with capital letter is public
 type Vertex struct {
 	X float64
 	Y float64
+}
+
+func cosa() {
+	//defer evaluates when called, but executes until its parent function returns
+	defer fmt.Println("Goodbye world")
+	fmt.Println("Hello world")
+	fmt.Printf("%d\n", 10)
+	fmt.Println(time.Now())
+	rand.Seed(10)
+	fmt.Printf("%d\n", rand.Intn(10))
+	fmt.Printf("%f\n", math.Pi)
+	fmt.Printf("%d\n", add(2, 2))
+
+	a, b, c := swap("Hello", "world", 5)
+
+	fmt.Printf("%d %s %s\n", a, b, c)
+
+	fmt.Printf("%d\n", square(4))
+
+	var sq float64 = 8596
+	fmt.Printf("My result: %f\n", sqrt(sq))
+	fmt.Printf("math.Sqrt result: %f\n", math.Sqrt(sq))
 }
 
 func add(x int, y int) int {
@@ -89,32 +112,93 @@ func structs() {
 
 func arrs() {
 	//var list [10]int
+	var arr = [10]int{1, 2, 3, 4, 5}
 	hello := "hello"
 	fmt.Printf("%s\n", hello)
+	//Slice: arr[0:6]
+	//Includes lower bound but excludes upper bound
+	fmt.Printf("%d\n", arr[0:1])
+}
+
+func fMake() {
+	a := make([]int, 4)
+	a[3] = 1
+	printSlice("a", a)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
+
+func mat() {
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	//board[0][0] = "X"
+	//board[2][2] = "O"
+	board[1][2] = "X"
+	//board[1][0] = "O"
+	//board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+func arrAppend() {
+	var arr []int
+	arr = append(arr, 1, 2, 3)
+	fmt.Println(arr)
+}
+
+func forEach() {
+	num := []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+	//foreach with variable of the value of the array at index i
+	for i, val := range num {
+		fmt.Printf("2^%d = %d\n", i, val)
+	}
+
+	//foreach with no additional varaible
+	for i := range num {
+		fmt.Printf("2^%d = 0\n", i)
+	}
+
+	//foreach without using index, only values in collection
+	for _, val := range num {
+		fmt.Printf("%d\n", val)
+	}
+}
+
+func pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+
+	for y := range pic {
+
+		pic[y] = make([]uint8, dx)
+
+		for x := range pic[y] {
+			pic[y][x] = uint8((x + y) / 2)
+		}
+	}
+	return pic
 }
 
 func main() {
-	//defer evaluates when called, but executes until its parent function returns
-	defer fmt.Println("Goodbye world")
-	fmt.Println("Hello world")
-	fmt.Printf("%d\n", 10)
-	fmt.Println(time.Now())
-	rand.Seed(10)
-	fmt.Printf("%d\n", rand.Intn(10))
-	fmt.Printf("%f\n", math.Pi)
-	fmt.Printf("%d\n", add(2, 2))
+	//cosa()
+	//pointers()
+	//structs()
+	//arrs()
+	//fMake()
+	//mat()
+	//arrAppend()
+	//forEach()
 
-	a, b, c := swap("Hello", "world", 5)
-
-	fmt.Printf("%d %s %s\n", a, b, c)
-
-	fmt.Printf("%d\n", square(4))
-
-	var sq float64 = 8596
-	fmt.Printf("My result: %f\n", sqrt(sq))
-	fmt.Printf("math.Sqrt result: %f\n", math.Sqrt(sq))
-
-	pointers()
-	structs()
-	arrs()
+	fmt.Println(pic(5, 5))
 }
---
+++ labs/lab0.1/lab01.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE * fp;
+    char c;
+    //char buffer[50];
+    int i = 0;
+    int j = 0;
+    int hasQuote = 0;
+    fp = fopen ("lab01.c", "r+");
+    //Get the number of chars in file
+    while(1)
+    {
+        c = fgetc(fp);
+        if(feof(fp))
+        {
+            break;
+        }
+        i++;
+    }
+    fseek(fp, 0, SEEK_SET);
+    char *buffer = (char *)malloc(sizeof(char)*i); 
+    //Fill buffer of size i with i chars
+    while(1)
+    {
+        c = fgetc(fp);
+        if(feof(fp))
+        {
+            break;
+        }
+        buffer[j] = c;
+        j++;
+    }
+    //Manipulate file now that we have it in array
+    for(int k = 0; k < i; k++)
+    {
+        if(buffer[k] == 34)
+        {
+            hasQuote = !hasQuote;
+        }
+        else
+        {
+            if(((buffer[k] == '/') && (buffer[k+1] == '/')) && !hasQuote)
+            {
+                while(1)
+                {
+                    fseek(fp, k, SEEK_SET);
+                    fputc(' ', fp);
+                    if(buffer[k] == 10)
+                    {
+                        fseek(fp, k, SEEK_SET);
+                        fputc(' ', fp);
+                        break;
+                    }
+                    k++;
+                }
+            }
+            if((buffer[k] == '/') && (buffer[k+1] == '*'))
+            {
+                // Continue reading until */ is found
+                while(1)
+                {
+                    fseek(fp, k, SEEK_SET);
+                    fputc(' ', fp);
+                    if((buffer[k-1] == '*') && (buffer[k] == '/'))
+                    {
+                        break;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+    fclose(fp);
+    return(0);
+}
