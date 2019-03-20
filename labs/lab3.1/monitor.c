97c5fe6d7194a85533f881a10591b52fb5ef7dc6
Carlos <carloscabello364@gmail.com>
2019-03-20 14:15:46 -0600

    Lab 3.1 fix

--- labs/lab3.1/monitor.c
+++ labs/lab3.1/monitor.c
@@ -89,8 +89,14 @@ void displayInotifyEvent(struct inotify_event *i)
     if (i->mask & IN_DELETE_SELF)   infof("IN_DELETE_SELF\n");
     if (i->mask & IN_MOVED_FROM)    infof("IN_MOVED_FROM\n");
     if (i->mask & IN_MOVED_TO)      infof("IN_MOVED_TO\n");
+    if (i->mask & IN_ISDIR)         infof("IN_ISDIR\n");
 
     if ((i->mask & IN_CREATE) && (i->mask & IN_ISDIR))
+    {
+        warnf("Adding %s to inotify FD.\n\n", i->name);
+        nftw(path, nftwFunc, 20, flags); 
+    }
+    if ((i->mask & IN_MOVED_TO) && (i->mask & IN_ISDIR))
     {
         warnf("Adding %s to inotify FD.\n\n", i->name);
         nftw(path, nftwFunc, 20, flags); 
@@ -100,24 +106,29 @@ void displayInotifyEvent(struct inotify_event *i)
         warnf("Directory with watch descriptor %d was removed from inotify FD.\n", i->wd);
         inotify_rm_watch(inotifyFd, i->wd);
     }
+    if (i->mask & IN_MOVE_SELF)
+    {
+        warnf("Directory with watch descriptor %d was removed from inotify FD.\n", i->wd);
+        inotify_rm_watch(inotifyFd, i->wd);
+    }
     if (i->cookie > 0)
     {
         if(cookie1 == 0)
         {
             cookie1 = i->cookie;
+            errorf("Cookie 1 set: %d\n", cookie1);
         }
         else if(cookie2 == 0)
         {
             cookie2 = i->cookie;
-        }
-        if(cookie1 != i->cookie && cookie2 != i->cookie)
-        {
-            cookie1=0;
-            cookie2=0;
+            errorf("Cookie 2 set: %d\n", cookie2);
         }
         if(cookie1 == cookie2)
         {
             warnf("File was renamed\n");
+            cookie1 = 0;
+            cookie2 = 0;
+            errorf("Cookies were reset\n");
         }
     }
 }
@@ -126,7 +137,7 @@ int nftwFunc(const char *fpath, const struct stat *sb, int tflag, struct FTW *ft
 {
     if(tflag == FTW_D || tflag == FTW_DP)
     {
-        int wd = inotify_add_watch(inotifyFd, fpath, IN_CREATE|IN_DELETE|IN_DELETE_SELF|IN_MOVED_FROM|IN_MOVED_TO);
+        int wd = inotify_add_watch(inotifyFd, fpath, IN_CREATE|IN_DELETE|IN_DELETE_SELF|IN_MOVED_FROM|IN_MOVED_TO|IN_ISDIR|IN_MOVE_SELF);
         if (wd == -1)
         {
             panicf("inotify_add_watch");
